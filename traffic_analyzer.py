d6808e297c8d1556a2fd66c0dca59e90f3c6a5c4
Denis <denis.savr@mail.ru>
2017-05-01 22:15:44 +0300

    Revamped code to use iterator, improved proto_dict structure, changed labels to russian

--- traffic_analyzer.py
+++ traffic_analyzer.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import division
 import socket
 import json
@@ -42,7 +43,7 @@ def inspect_packet(p):
 
             try:
                 # Check if we have unparsable data above transport layer like HTTPS in case of TCP
-                if data and data.name in ('Raw', 'Padding'):
+                if data and data.name in ('Raw', 'Payload'):
                     try:
                         al_proto_name = socket.getservbyport(p.payload.sport)
                     except socket.error:
@@ -125,141 +126,157 @@ ip_protos = load_obj('ip_protos')
 #     print(ether_json)
 #     exit()
 
-# packet_dump = rdpcap('mac.pcap')
-# packet_dump = rdpcap('new_dump.pcap')
-# packet_dump = rdpcap('dump.pcap')
-packet_dump = rdpcap('mpls-basic.pcap')
-# packet_dump = rdpcap('conference.pcap')
+# filename = 'new_dump.pcap'
+# filename = 'mac.pcap'
+filename = 'mac2.pcap'
+# filename = 'dump.pcap'
+# filename = 'mpls-basic.pcap'
+# filename = 'conference.pcap'
 
-# Get protocol summary
-summary = repr(packet_dump).strip('<').strip('>').split()[1:]
-quantity_dict = {}
-for item in summary:
-    p, q = item.split(':')
-    quantity_dict[p] = int(q)
+packet_dump = PcapReader(filename)
 
-total_quantity = len(packet_dump)
-tcp_udp_quantity = quantity_dict['TCP'] + quantity_dict['UDP']
-other_quantity = quantity_dict['Other']
+# Get protocol summary if we use rdpcap()
+# summary = repr(packet_dump).strip('<').strip('>').split()[1:]
+# quantity_dict = {}
+# for item in summary:
+#     p, q = item.split(':')
+#     quantity_dict[p] = int(q)
+
+# total_quantity = sum(quantity_dict.values())
+# tcp_udp_quantity = quantity_dict['TCP'] + quantity_dict['UDP']
+# other_quantity = quantity_dict['Other']
+
+total_quantity = 0
+tcp_udp_quantity = 0
+other_quantity = 0
 
 proto_dict = {}
 frame_sizes = defaultdict(lambda: 0)
-packet_time_size = OrderedDict()
+packet_interval_time_speed = OrderedDict()
+
+packet_dump_time = PcapReader(filename)
+
+enough = 0
+
+first_time = last_time = next(packet_dump_time)
+for last_time in packet_dump_time:
+    enough += 1
+    if enough == 5000:
+        break
+        # pass
+
+first_time = first_time.time
+last_time = last_time.time
+
+time_period = last_time - first_time
+# print(time_period)
+
+interval = time_period / 20
+
+left_border = first_time
+right_border = first_time + interval
+
+interval_volume = 0
+pos = 1
+enough = 0
 
 for packet in packet_dump:
-    packet_size = len(packet)
+    try:
+        total_quantity += 1
 
-    if packet_size <= 64:
-        frame_sizes[64] += 1
-    elif 64 < packet_size <= 128:
-        frame_sizes[128] += 1
-    elif 128 < packet_size <= 256:
-        frame_sizes[256] += 1
-    elif 256 < packet_size <= 512:
-        frame_sizes[512] += 1
-    elif 512 < packet_size <= 1518:
-        frame_sizes[1518] += 1
-    elif packet_size < 1518:
-        frame_sizes[1600] += 1
+        # Regulate the quantity of parsed packets
+        enough += 1
+        if enough == 5000:
+            break
 
-    packet_time_size[packet.time] = packet_size
+        packet_size = len(packet)
 
-    inspection_result = inspect_packet(packet)
+        if packet_size <= 64:
+            frame_sizes[64] += 1
+        elif 64 < packet_size <= 128:
+            frame_sizes[128] += 1
+        elif 128 < packet_size <= 256:
+            frame_sizes[256] += 1
+        elif 256 < packet_size <= 512:
+            frame_sizes[512] += 1
+        elif 512 < packet_size <= 1518:
+            frame_sizes[1518] += 1
+        elif packet_size < 1518:
+            frame_sizes[1600] += 1
 
-    # print(repr(packet))
-    # print(inspection_result)
-    # print('')
+        # print(left_border, right_border)
+        # print(packet.time)
+        # print('')
 
-    # UDP or TCP transport layers
-    if not isinstance(inspection_result, str):
-        if len(inspection_result) == 3:
-            # Divide proto to internet and transport layer protocols
-            i_proto, t_proto, al_proto = inspection_result
-            if i_proto in proto_dict:
-                if t_proto in proto_dict[i_proto]:
-                    if al_proto in proto_dict[i_proto][t_proto]:
-                        proto_dict[i_proto][t_proto][al_proto].update({packet.time: packet_size})
-                    else:
-                        proto_dict[i_proto][t_proto].update({al_proto: {packet.time: packet_size}})
-                else:
-                    proto_dict[i_proto].update({t_proto: {al_proto: {packet.time: packet_size}}})
-            else:
-                proto_dict[i_proto] = {t_proto: {al_proto: {packet.time: packet_size}}}
-        # Other transport layers
-        elif len(inspection_result) == 2:
-            i_proto, t_proto = inspection_result
-            if i_proto in proto_dict:
-                if t_proto in proto_dict[i_proto]:
-                    proto_dict[i_proto][t_proto].update({packet.time: packet_size})
-                else:
-                    proto_dict[i_proto][t_proto] = {packet.time: packet_size}
-            else:
-                proto_dict[i_proto] = {t_proto: {packet.time: packet_size}}
-    # All other protocols
-    else:
-        proto = inspection_result
-        if proto in proto_dict:
-            proto_dict[proto].update({packet.time: packet_size})
+        if left_border <= packet.time < right_border:
+            interval_volume += packet_size
         else:
-            proto_dict[proto] = {packet.time: packet_size}
+            interval_middle_time = first_time + interval * (pos + 0.5)
+            packet_interval_time_speed[interval_middle_time] = interval_volume / 1024 / interval
+            interval_volume = packet_size
+            pos += 1
+            left_border = first_time + interval * (pos - 1)
+            right_border = first_time + interval * pos
 
-# for proto, val in proto_dict.items():
-#     if isinstance(val.values()[0], dict) and isinstance(val.values()[0].keys()[0], float):
-#         print(proto)
-#         for item, item_val in val.items():
-#             # print('\t' + repr((item, len(item_val.items()), item_val.items())))
-#             print('\t' + repr((item, len(item_val.items()))))
-#     else:
-#         # print(proto, len(val.items()), val.items())
-#         print(proto, len(val.items()))
-#     print('')
+        inspection_result = inspect_packet(packet)
+
+        # UDP or TCP transport layers
+        if not isinstance(inspection_result, str):
+            if len(inspection_result) == 3:
+                tcp_udp_quantity += 1
+                # Divide proto to internet and transport layer protocols
+                i_proto, t_proto, al_proto = inspection_result
+                if i_proto in proto_dict:
+                    if t_proto in proto_dict[i_proto]:
+                        if al_proto in proto_dict[i_proto][t_proto]:
+                            proto_dict[i_proto][t_proto][al_proto]['quantity'] += 1
+                            proto_dict[i_proto][t_proto][al_proto]['volume'] += packet_size
+                        else:
+                            proto_dict[i_proto][t_proto].update({al_proto: {packet.time: packet_size}})
+                            proto_dict[i_proto][t_proto].update({al_proto: {'quantity': 1, 'volume': packet_size}})
+                    else:
+                        proto_dict[i_proto].update({t_proto: {al_proto: {'quantity': 1, 'volume': packet_size}}})
+                else:
+                    proto_dict[i_proto] = {t_proto: {al_proto: {'quantity': 1, 'volume': packet_size}}}
+            # Other transport layers
+            elif len(inspection_result) == 2:
+                other_quantity += 1
+                i_proto, t_proto = inspection_result
+                if i_proto in proto_dict:
+                    if t_proto in proto_dict[i_proto]:
+                        proto_dict[i_proto][t_proto]['quantity'] += 1
+                        proto_dict[i_proto][t_proto]['volume'] += packet_size
+                    else:
+                        proto_dict[i_proto][t_proto] = {'quantity': 1, 'volume': packet_size}
+                else:
+                    proto_dict[i_proto] = {t_proto: {'quantity': 1, 'volume': packet_size}}
+        # All other protocols
+        else:
+            other_quantity += 1
+            proto = inspection_result
+            if proto in proto_dict:
+                proto_dict[proto]['quantity'] += 1
+                proto_dict[proto]['volume'] += packet_size
+            else:
+                proto_dict[proto] = {'quantity': 1, 'volume': packet_size}
+    except IndexError:
+        # Some packets have crippled format so the len function and inspection doesn't work
+        # print('Achtung!')
+        pass
 
 end_time = datetime.now()
 elapsed_time = end_time - start_time
 print('Elapsed time: ' + str(elapsed_time))
 
-# print(frame_sizes)
-# exit()
-# for proto in proto_dict:
-#     print(proto, proto_dict[proto])
-
-
-time_period = packet_time_size.keys()[-1] - packet_time_size.keys()[0]
-# print(time_period)
-
-# interval = time_period / 20
-interval = 1
-
-packet_interval_time_size = OrderedDict()
-for pos in range(1, int(time_period)):
-    interval_speed = 0
-
-    left_border = packet_time_size.keys()[0] + interval * (pos - 1)
-    right_border = packet_time_size.keys()[0] + interval * pos
-
-    interval_middle_time = packet_time_size.keys()[0] + interval * (pos + 0.5)
-
-    for ptime in packet_time_size:
-        if left_border <= ptime <= right_border:
-            interval_speed += packet_time_size[ptime] * 8 / 1024 / interval
-
-    packet_interval_time_size[interval_middle_time] = interval_speed
-
-# print(packet_interval_time_size)
-# print(sum(packet_interval_time_size.values()), sum(packet_time_size.values()))
-
-# dark_rotate_style = pygal.style.RotateStyle('#9e6ffe')
-# chart = pygal.StackedLine(fill=True, interpolate='cubic', style=dark_rotate_style)
-
 # Create visualizations of gathered statistics
 pie_chart = pygal.Pie(formatter=lambda x: '{:.1f}%'.format(x * 100 / total_quantity))
-pie_chart.title = 'Network traffic protocol distribution'
+pie_chart.title = u'Распределение протоколов'
 
 pie_chart_no_ip = pygal.Pie(formatter=lambda x: '{:.1f}%'.format(x * 100 / other_quantity))
-pie_chart_no_ip.title = 'Network traffic protocol distribution without IP'
+pie_chart_no_ip.title = u'Распределение протоколов без IPv4 и IPv6'
 
-pie_chart_tcp_udp = pygal.Pie(formatter=lambda x: '{:.1f}%'.format(x * 100 / tcp_udp_quantity))
-pie_chart_tcp_udp.title = 'TCP and UDP application protocols'
+pie_chart_tcp_udp = pygal.Pie(formatter=lambda x: '{:.3f}%'.format(x * 100 / tcp_udp_quantity))
+pie_chart_tcp_udp.title = u'Распределение протоколов уровня приложений для TCP и UDP'
 
 if 'IP' in proto_dict and 'IPv6' in proto_dict:
     ip_ipv6_tags = True
@@ -268,8 +285,8 @@ else:
 
 for proto, sub_proto in proto_dict.items():
     # Check if the proto's sub_proto is a dictionary of packets
-    if isinstance(sub_proto.keys()[0], float):
-        quantity = len(sub_proto)
+    if isinstance(sub_proto.values()[0], int):
+        quantity = sub_proto['quantity']
     # Parse proto's sub_proto
     else:
         quantity = []
@@ -278,16 +295,16 @@ for proto, sub_proto in proto_dict.items():
                 s_quantity = []
                 count = 0
                 for s_p in sub_proto[item]:
-                    count += len(sub_proto[item][s_p])
+                    count += sub_proto[item][s_p]['quantity']
                     s_quantity.append(
-                        {'value': len(sub_proto[item][s_p]), 'label': s_p})
+                        {'value': sub_proto[item][s_p]['quantity'], 'label': s_p})
                 if ip_ipv6_tags:
                     pie_chart_tcp_udp.add(proto + ': ' + item, s_quantity)
                 else:
                     pie_chart_tcp_udp.add(item, s_quantity)
-                quantity.append({'value': count, 'label': item}) #, 'color': get_rand_color()})
+                quantity.append({'value': count, 'label': item})
             else:
-                quantity.append({'value': len(sub_proto[item].values()), 'label': item})
+                quantity.append({'value': sub_proto[item]['quantity'], 'label': item})
     pie_chart.add(proto, quantity)
     if proto not in ('IP', 'IPv6'):
         pie_chart_no_ip.add(proto, quantity)
@@ -298,66 +315,50 @@ pie_chart_tcp_udp.render_to_file('tcp_and_udp.svg')
 
 # Create config for traffic speed and time chart
 st_chart_config = pygal.Config()
-st_chart_config.title = 'Traffic IO graph'
+st_chart_config.title = u'Использование ресурсов сети'
 st_chart_config.show_legend = False
-st_chart_config.x_title = 'Time hh:mm:ss'
-st_chart_config.y_title = 'Speed in kbit/s'
+st_chart_config.x_title = u'Время в формате чч:мм:сс'
+st_chart_config.y_title = u'Скорость в КБ/с'
 st_chart_config.x_label_rotation = 30
-# my_config.interpolate = 'cubic'
 st_chart_config.interpolate = 'hermite'
 st_chart_config.interpolation_parameters = {'type': 'kochanek_bartels', 'b': -1, 'c': 1, 't': 1}
-# my_config.interpolation_parameters = {'type': 'cardinal', 'c': .75}
+st_chart_config.formatter = lambda x: '{:.2f}'.format(x)
 st_chart_config.show_y_guides = True
 # my_config.show_x_guides = True
-st_chart_config.width = 1000
+# st_chart_config.width = 800
 
 speed_time_chart = pygal.Line(st_chart_config)
 
 # Define value interval for abscissa
-x_int = len(packet_interval_time_size) // 6
+x_int = len(packet_interval_time_speed) // 6
 
 get_x_label = x_labels_factory(x_int)
-speed_time_chart.x_labels = map(get_x_label, map(datetime.fromtimestamp, packet_interval_time_size.keys()))
+speed_time_chart.x_labels = map(get_x_label, map(datetime.fromtimestamp, packet_interval_time_speed.keys()))
 
 get_interval_value = values_factory(x_int)
-interval_values = [get_interval_value(s, t) for t, s in packet_interval_time_size.items()]
+interval_values = [get_interval_value(s, t) for t, s in packet_interval_time_speed.items()]
 
 speed_time_chart.add("", interval_values)
 speed_time_chart.render_to_file('time_chart.svg')
 
 # Create config for traffic speed and time chart
 f_bar_config = pygal.Config()
-f_bar_config.title = 'Frame size distribution'
+f_bar_config.title = u'Распределение по размеру кадров'
 f_bar_config.show_legend = False
-f_bar_config.x_title = 'Size groups in bytes'
-f_bar_config.y_title = 'Number of frames'
-# f_bar_config.x_label_rotation = 30
-# my_config.interpolate = 'cubic'
-# f_bar_config.interpolate = 'hermite'
-# f_bar_config.interpolation_parameters = {'type': 'kochanek_bartels', 'b': -1, 'c': 1, 't': 1}
-# my_config.interpolation_parameters = {'type': 'cardinal', 'c': .75}
+f_bar_config.x_title = u'Размерные группы в байтах'
+f_bar_config.y_title = u'Количество кадров'
 f_bar_config.show_y_guides = True
 f_bar_config.rounded_bars = 10
 # my_config.show_x_guides = True
-f_bar_config.width = 1000
+# f_bar_config.width = 1000
 f_bar_config.formatter = lambda x: '{:.1f}%'.format(x * 100 / total_quantity)
-
-# style = pygal.style.LightenStyle
-# dark_lighten_style = style('#564324')
 
 frame_sizes_bar = pygal.Bar(f_bar_config)
 
 frame_sizes_bar.x_labels = sorted(frame_sizes.keys())
 
-# x_range = len(frame_sizes)
-# for pos, (size, value) in enumerate(sorted(frame_sizes.items())):
-#     x_range_labels = [0]*x_range
-#     x_range_labels[pos] = value
-#     frame_sizes_bar.add(str(size), x_range_labels)
-
-frame_sizes_values = [
-    {'value': val, 'color': get_rand_color()} for
-    item, val in sorted(frame_sizes.items(), key=itemgetter(0))]
+frame_sizes_values = [{'value': val, 'color': get_rand_color()} for item, val in
+                      sorted(frame_sizes.items(), key=itemgetter(0))]
 frame_sizes_bar.add("", frame_sizes_values)
 
 frame_sizes_bar.render_to_file('frame_sizes.svg')
